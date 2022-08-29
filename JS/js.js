cae4bb18e4256a8850a096bd19e8acf6afffd97a
Julian <juliandelgadoconde@gmail.com>
2022-08-29 14:47:24 -0500

    modificacion

--- JS/js.js
+++ JS/js.js
@@ -10,26 +10,11 @@ function cambio() {
     var main = document.querySelector('.main');
     main.classList.toggle('change-dark');
 
-    var cajaGris = document.querySelector('.CajaGris');
-    cajaGris.classList.toggle('change-dark');
 
-    var cajaGris2 = document.querySelector('.CajaGris2');
-    cajaGris2.classList.toggle('change-dark');
 
-    var cajaGris3 = document.querySelector('.CajaGris3');
-    cajaGris3.classList.toggle('change-dark');
 
-    var info = document.querySelector('.footer');
-    footer.classList.toggle('change-dark');
 
-    var hs1 = document.querySelector('h1');
-    hs.classList.toggle('change-dark');
-    
-    var hs3 = document.querySelector('h3');
-    hs3.classList.toggle('change-dark');
 
-    var footer = document.querySelector('.footer');
-    footer.classList.toggle('change-dark');
 
     var titulo = document.querySelector('.titulo');
     titulo.classList.toggle('change-dark');
